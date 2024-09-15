1f492f384705313ee1e8e868ec5472bf87169359
AlexFern17 <123132428+AlexFern17@users.noreply.github.com>
2024-09-15 13:32:06 -0500

    Created basic html for weather app, added testing css formatting

--- src/Weather.js
+++ src/Weather.js
@@ -2,5 +2,31 @@ import React from "react";
 import "./Weather.css";
 
 export default function Weather() {
-  return <div className="Weather">Hello from Weather</div>;
+  return (
+    <div className="Weather">
+      <div className="weatherContainer">
+        <div className="row">
+          <div className="col-9">Search form</div>
+          <div className="col-3">Submit button</div>
+        </div>
+        <div className="row">
+          <div className="col-3">88°C</div>
+          <div className="col-6">
+            <ul>
+              <li>Precipitation</li>
+              <li>Humidity</li>
+              <li>Wind</li>
+            </ul>
+          </div>
+          <div className="col-3">
+            <ul>
+              <li>Location</li>
+              <li>Date</li>
+              <li>Condition</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
